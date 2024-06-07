614b2816c2914e12a023cb4d4a2b4d9da6103799
jack <redcatmiss@126.com>
2024-06-07 12:55:16 +0800

    minor updates

--- vat_test.go
+++ vat_test.go
@@ -3,21 +3,24 @@ package main
 import (
 	"context"
 	"github.com/UniB-e-e/unibee-go-client"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 func TestVatSetup(t *testing.T) {
-	unibee.ApiKey = "EUXAgwv3Vcr1PFWt2SgBumMHXn3ImBqM"
+	unibee.ApiKey = "5fshy9X6e3Wn0a6xjyRLMq2GGNM4sTEf"
 	unibee.Host = UniBeeStageUrl
 	ctx := context.Background()
 	configuration := unibee.NewConfiguration()
 	apiClient := unibee.NewAPIClient(configuration)
 	t.Run("Test Vat Setup", func(t *testing.T) {
-		apiClient.Vat.VatSetupGatewayPost(ctx).UnibeeApiMerchantVatSetupGatewayReq(unibee.UnibeeApiMerchantVatSetupGatewayReq{
+		resp, httpRes, err := apiClient.Vat.VatSetupGatewayPost(ctx).UnibeeApiMerchantVatSetupGatewayReq(unibee.UnibeeApiMerchantVatSetupGatewayReq{
 			IsDefault:   unibee.Bool(true),
-			Data:        "${YOUR_VAT_SENSE_KEY}",
+			Data:        "d9d57f2212cba7e286b3fb9cbb2ad419",
 			GatewayName: "vatsense",
-		})
-
+		}).Execute()
+		require.Nil(t, err)
+		require.NotNil(t, resp)
+		require.Equal(t, 200, httpRes.StatusCode)
 	})
 }
